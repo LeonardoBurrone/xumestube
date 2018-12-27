1635c73787efbca20d5e7473e17ccf7524116f01
leonardo <leonardo.burrone@matera.com>
2018-12-27 10:03:14 -0200

    Section 2, until lesson 32

--- src/componentes/videoPlayer.js
+++ src/componentes/videoPlayer.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Advertisement } from 'semantic-ui-react';
+import { Advertisement, Embed } from 'semantic-ui-react';
 
 const VideoPlayer = props => {
     return (
         <div className="video-player">
-            <Advertisement style={{'height': '433px'}} unit='top banner' test='Escolha um vídeo para reproduzir' />
+            {
+                !props.video.id && (
+                    <Advertisement style={{'height': '433px'}} unit='top banner' test='Escolha um vídeo para reproduzir' />
+                )
+            }
+            {
+                props.video.id && (
+                    <div>
+                        <Embed id={props.video.id.videoId} source='youtube' placeholder/>
+                        <p className='video-titulo'>{props.video.snippet.title}</p>
+                        <p className='video-descricao'>{props.video.snippet.description}</p>
+                    </div>
+                )
+            }
         </div>
         
     )
